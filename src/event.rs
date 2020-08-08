2c60d5a87161c02c8ca8c70b0d3c6b9ef28e663a
William Hua <william@attente.ca>
2020-08-08 11:13:04 -0400

    CardEvents

--- src/event.rs
+++ src/event.rs
@@ -1,4 +1,4 @@
-use crate::CardLocation;
+use crate::{CardInstance, CardLocation, OpaquePointer, State};
 
 #[cfg(feature = "bindings")]
 use wasm_bindgen::prelude::wasm_bindgen;
@@ -9,8 +9,31 @@ use wasm_bindgen::prelude::wasm_bindgen;
 )]
 #[derive(serde::Serialize, serde::Deserialize)]
 #[serde(tag = "type", content = "payload")]
-pub enum CMSEvent {
+pub enum CardEvent<S: State> {
+    #[serde(bound = "S: State")]
+    NewCard {
+        instance: CardInstance<S>,
+        location: CardLocation,
+        #[serde(rename = "isAttachment")]
+        is_attachment: bool,
+    },
+    NewPointer {
+        pointer: OpaquePointer,
+        location: CardLocation,
+    },
+    #[serde(bound = "S: State")]
+    ResetCard {
+        instance: CardInstance<S>,
+        location: CardLocation,
+    },
+    #[serde(bound = "S: State")]
+    ModifyCard {
+        instance: CardInstance<S>,
+        location: CardLocation,
+    },
+    #[serde(bound = "S: State")]
     MoveCard {
+        instance: Option<CardInstance<S>>,
         from: CardLocation,
         to: CardLocation,
     },
